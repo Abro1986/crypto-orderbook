1a85ae5dd0a7b698e153daaa15f94f4d82e210ad
Abro1986 <AndrewBroestl@gmail.com>
2018-10-20 16:05:19 -0600

    combining bid prices into a buy order book

--- server.js
+++ server.js
@@ -32,33 +32,50 @@ app.get('/api/bittrex', function(req, res) {
 			let dataPolo = JSON.parse(response)
 			trexPrice = dataTrex.result
 			poloPrice = dataPolo.bids
-			for (i = 0; i < dataTrex.result.buy.length; i++) {
-				console.log(trexPrice.buy[i].Rate.toString().split('').splice(0, 7).join(''))
-//				console.log(trexPrice.buy[i].Rate)
-//				newRate = trexPrice.buy[i].Rate.split('').pop([5])
-			}
-			console.log(dataPolo.bids[0][0])
-			for (i = 0; i < dataTrex.result.buy.length; i++) {
-				// console.log(trexPrice.buy[i].Rate)
-				for (j = 0; j < poloPrice.length; j++) {
-					myOrderBook = []
-					if (poloPrice[j][0] === trexPrice.buy[i].Rate) {
-						console.log('match')
-					}
-				}
-			};
+			console.log(dataPolo)
+			console.log(dataTrex.result.sell)
 			data = {
 				trex: dataTrex.result.buy,
 				polo: dataPolo.bids
+			};
+			console.log(data.polo.length);
+			sellOrderBook= []
+			buyOrderBook = []
+			for (i = 0; i < dataTrex.result.buy.length; i++) {
+				data.trex[i].Rate = trexPrice.buy[i].Rate.toString().split('').splice(0, 8).join('')
+			}
+			for (i = 0; i < data.polo.length; i++) {
+				data.polo[i][0] = data.polo[i][0].toString().split('').splice(0, 8).join('')
+//				console.log(data.polo[i][0])
+			}
+			for (i = 0; i < dataTrex.result.buy.length; i++) {
+				// console.log(trexPrice.buy[i].Rate)
+				for (j = 0; j < poloPrice.length; j++) {
+					
+						combinedVolume = data.polo[j][1] + data.trex[i].Quantity
+					if (data.polo[j][0] === data.trex[i].Rate) {
+						// console.log(data.polo[j][1])
+						// console.log(data.trex[i].Quantity)
+						// console.log(combinedVolume)
+						bidData = {
+							Rate: data.polo[j][0],
+							Quantity: combinedVolume,
+							key: i
+						}
+//						console.log(bidData)
+						buyOrderBook.push(bidData) 
+//						console.log(myOrderBook)
+					}
+				}
 			};
 			return data
 	}).then((data) => {
 		res.send(data)
 	})
-	
-	})
 	.catch((err) => {
 		res.send(err)
+	
+	})
 	})
 	
 });
